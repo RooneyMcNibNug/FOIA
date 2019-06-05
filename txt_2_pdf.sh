180f5909292388f9ad06e3722f629cfa53a119bd
JA <Jakea350@protonmail.com>
2019-06-05 16:05:00 -0500

    Added comments

--- txt_2_pdf.sh
+++ txt_2_pdf.sh
@@ -1,3 +1,7 @@
+#Quick way to convert a .txt to a .pdf with enscript & ghostscript. 
+#This is for a single file input, but plan to creating an all-directory
+#script to convert all files within then run pdfunite.
+
 #!/bin/bash
 
 STAMP=`date '+%m_%d__%H_%M_%S'`
@@ -7,5 +11,5 @@ echo Please input the filename - minus .txt - you wish to convert to .pdf
 read varname
 
 enscript -p output.ps $varname.txt
-sed -i '/fmodstr/d' output.ps
+sed -i '/fmodstr/d' output.ps #remove postscript timestamp to avoid confusion
 ps2pdf output.ps $STAMP.pdf
